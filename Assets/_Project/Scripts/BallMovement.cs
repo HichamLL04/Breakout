73df0ba2fdc799dad7f415b174eb389068fb2316
Kaiser <hichamlaaouni52@gmail.com>
2026-01-18 23:00:43 +0100

    feat: lanzamiento de la pelota
    Al activarse el trow lanza la pelota. Para lanzarla es el correspondiente botón de mando o en pc, la espaciadora o el enter.

--- Assets/_Project/Scripts/BallMovement.cs
+++ Assets/_Project/Scripts/BallMovement.cs
@@ -5,6 +5,7 @@ public class BallMovement : MonoBehaviour
 {
     [SerializeField] GameObject pala;
     [SerializeField] float offsetY = 0.5f;
+    [SerializeField] float fuerzaTiro = 2f;
     float nuevaY;
     bool hasStarted = false;
     Rigidbody2D myRb;
@@ -24,6 +25,31 @@ public class BallMovement : MonoBehaviour
         {
             float nuevaX = pala.transform.position.x + offsetX;
             myRb.position = new Vector2(nuevaX, nuevaY);
+        }
+    }
+
+    void OnTrow()
+    {
+        if (!hasStarted)
+        {
+            hasStarted = true;
+            myRb.linearVelocity = new Vector2(RandomY(), fuerzaTiro);
+            Debug.Log("Bola lanzada");
+        }
+    }
+
+    float RandomY()
+    {
+        float giro = Random.Range(0.1f, 1f);
+        return giro;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Lo dejo asi de momento. La intencion es que cada vez que golpe al jugador
+            // este agregue fuerza a y para evitar que sea tan recto como pasaba en el pong.
         }
     }
 }
