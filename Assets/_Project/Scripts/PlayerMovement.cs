af54e62417e29522417797bae5c1604fdb5e8345
Kaiser <hichamlaaouni52@gmail.com>
2026-01-18 21:01:08 +0100

    feat: Movimiento del jugador
    Ahora el jugador puede moverse de izquierda a derecha pero bloqueando la posibilidad de que gire o suba/baje

---
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    Rigidbody2D myRb;
+    Vector2 moveInput;
+    void Start()
+    {
+      myRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Vector2 playerVelocity = new Vector2(moveInput.x * speed, 0f);
+        myRb.linearVelocity = playerVelocity;
+    }
+}
