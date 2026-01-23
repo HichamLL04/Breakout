53eb015761a9543df9834bd4facc54aeda061529
Kaiser <hichamlaaouni52@gmail.com>
2026-01-23 23:53:08 +0100

    feat: canvas gameOver

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject game;
+    [SerializeField] GameObject gameOver;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
